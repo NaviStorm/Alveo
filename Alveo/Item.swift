4c0951d426a66226d923f3b2114923019a5b79d6
Andreu Ascensio Thierry <thierry.andreu@me.com>
2025-05-27 19:20:40 +0200

    Initial Commit

---
+++ Alveo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Alveo
+//
+//  Created by Thierry Andreu Asscensio on 27/05/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AlveoTests/AlveoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlveoTests.swift
+//  AlveoTests
+//
+//  Created by Thierry Andreu Asscensio on 27/05/2025.
+//
+
+import Testing
+@testable import Alveo
+
+struct AlveoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
